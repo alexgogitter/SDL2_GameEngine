f06c5e1d09215945f7ae0a9669a0b43a9f43d048
Matt Bannister <matt.bann@live.co.uk>
2022-09-11 19:23:05 +0100

    Moved Dll now working

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Window
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char* windowTitle = "OpenGL learning";
+
 int main(){
+
+
+	// Initalise GLFW for the window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // Spec what the window should have
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6); // Setting to version 4.6
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MACOSX
+#endif
+
+	// Create the window assign it to var window
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, windowTitle, NULL, NULL);
+	// ViewPort tell openGL the size of the rendering window
+	// First 2 params are where the window will be created
+
+	// Error
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
 
 
     std::cout << "Hello, world" << std::endl;
