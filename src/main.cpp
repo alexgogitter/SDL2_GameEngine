9a36c196ff20e5d436794e228e683dd5402e1939
Matt Bannister <matt.bann@live.co.uk>
2022-09-12 20:18:56 +0100

    Merge and changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
+#include "mapReader.h"
 #include "render.h"
 
 //Simple game design follows a Input -> Update -> draw Loop when running
+
+
+int mapReader::width, mapReader::height, mapReader::n;
 
 int main( int argc, char* argv[] )
 {
@@ -12,7 +17,6 @@ int main( int argc, char* argv[] )
     r.Renderer_Init();
     SDL_RenderClear(r.get_SDLRenderer());
     
-
     //Main loop flag
     bool quit = false;
 
@@ -22,7 +26,6 @@ int main( int argc, char* argv[] )
     //While application is running
     while( !quit )
     {   
-
         //Handle events on queue
         while( SDL_PollEvent( &e ) != 0 )
         {
@@ -36,16 +39,8 @@ int main( int argc, char* argv[] )
         
         SDL_RenderClear(r.get_SDLRenderer());
 
-#include <iostream>
-#include <vector>
-#include "mapReader.h"
-
-
         SDL_RenderPresent(r.get_SDLRenderer());
-int mapReader::width, mapReader::height, mapReader::n;
 
-
-        
     }
 
     /*need to work with a basic input -> update -> render System*/
@@ -56,39 +51,6 @@ int mapReader::width, mapReader::height, mapReader::n;
 
 	mapReader::Reader(map, mapWidth, mapHeight, mapName);
 	mapReader::printMap(map);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-
-}
-<<<<<<< HEAD
-	std::vector<std::vector<colorVals>> map;
-	int mapWidth, mapHeight;
-
-	std::string mapName = "res/maps/Map2.png";
-
-	mapReader::Reader(map, mapWidth, mapHeight, mapName);
-	mapReader::printMap(map);
-
-
-
-
-
-
-
-
-
 
 
     return 0;
